76fdef4a3ef6a4c5209673143eb975b5046a5473
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2022-11-17 19:18:54 -0600

    day10_11_13

---
+++ src/main/java/day10_11_12/Static.java
@@ -0,0 +1,93 @@
+package day10_11_12;
+
+public class Static {
+    //DAY 10/11/12
+
+     /*
+        1)   Example 1: Ask user to enter an integer.
+           If the integer is less than 100, tell user "Won!"
+           Otherwise, tell user "Lost!"
+   */
+    //tell some about "static block".   //static block nasil ve nereye yazilir class da?
+    //When you use while-loop ----------   execution is possible/impossible
+    //When you use do-while loop,-------- execution is possible/impossible
+    //infinite loops yapma yollari nelerdir?3 yol ogretildi bize
+    //How to create "constructor"
+    //What is the difference between "Constructors" and "Methods"
+    //How to create "static" variable and tell some about
+    //how to  access a static variable
+    // what is the Default values  for numeric data types,  for String and  for boolean?
+    //Java does not assign default value for -------------?
+    //'return' key word u hangi durumda methoda eklemek zorundasin ve methodun icinde nereye eklemelisin
+
+    /*       Example 1: Ask user to enter an integer.
+           If the integer is less than 100, tell user "Won!"
+           Otherwise, tell user "Lost!"
+   */
+
+    //Example 1: Type code to print integers from 3 to 6
+    //Example 2: Type code to print odd integers from 12 to 67 in the same line with a space between them
+
+    //4.Example: Type java code by using while loop,
+    //           Write a program that prompts the user to input an integer.
+    //           It should then find the sum of the digits of that number.
+    //           123 ==> 1+2+3 = 6
+
+ /*
+Example 5: Type java code by using while loop,
+   Write a program that prompts the user to input a number.
+   It should then print the multiplication table of that number.
+   3x1=3  3x2=6  3x3=9  3x4=12 3x5=15  3x6=18  3x7=21  3x8=24  3x9=27  3x10=30
+*/
+
+   /*
+         1.Example:Type code to get the output like
+                     Week: 1
+                       Day: 1
+                       Day: 2
+                       Day: 3
+                       .....
+                     Week: 2
+                       Day: 1
+                       Day: 2
+                       Day: 3
+                       ....
+                     Week: 3
+                       Day: 1
+                       Day: 2
+                       Day: 3
+                       ....
+
+         2.Example: Write a Java Program to Print Rectangle Star Pattern using For Loop
+                     ****
+                     ****
+                     ****
+         Note: Get the number of the rows and the columns from user
+
+ /*
+         3.Example: Type code to get the output like
+                     1
+                     1 2
+                     1 2 3
+                     1 2 3 4
+                     1 2 3 4 5
+     */
+
+    // Type code to print digits just in the decimal part of the given decimal number with an asterix.
+    // For example; 75.4238 ⇒ *4*2*3*8
+ /*
+
+ Username is "admin", Password is "pwd123"
+ Ask user to enter username and password
+ User should see "Enter your username and password" message
+ hIf user enters correct credentials, e should get "You are in your account!" message
+ Otherwise, he should see "Enter your username and password" message 3 times
+ After 3 times he should get "Your account is blocked" message
+
+ */
+    //Type code to check if a given integer is palindrome or not
+
+    //Palindrome: 121 <==> 121  123321 <==> 123321 nalan <==>nalan
+}
+
+
